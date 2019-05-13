6e35ea95044c0178596aa06b62765c2e1ab97beb
Conor <conorbrosnan10@gmail.com>
2019-05-13 23:22:51 +1000

    commandlisttests

--- src/CommandList/CommandList.java
+++ src/CommandList/CommandList.java
@@ -17,8 +17,8 @@ public class CommandList implements Iterable<Command> {
      * the count of the commands in the CommandList object
      * @return int of the count
      */
-    public int count(){
-        return commands.size()-1;
+    public int Count(){
+        return commands.size();
     }
 
     /**
--- src/Tests/CommandListTests.java
+++ src/Tests/CommandListTests.java
@@ -1,4 +1,47 @@
 package Tests;
 
-public class CommandListTests {
+import CommandList.CommandList;
+import Commands.Command;
+import Commands.Line;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CommandListTests {
+    private CommandList list;
+
+    @BeforeEach
+    public void beforeEach(){
+        list = null;
+        list = new CommandList();
+    }
+
+    @Test
+    public void testAddCommand(){
+        list.addCommand(new Line(1, 1, Color.BLACK));
+    }
+
+    @Test
+    public void testCount(){
+        addCommandsToList(list, 20);
+        assertEquals(20, list.Count());
+    }
+
+    @Test
+    public void testremoveLast(){
+        addCommandsToList(list, 20);
+        list.removeLastCommand();
+        assertEquals(19, list.Count());
+    }
+
+    private void addCommandsToList(CommandList list, int count){
+        Command cmd;
+        for(int i = 0; i < count; i++){
+            cmd = new Line(1, 1, Color.WHITE);
+            list.addCommand(cmd);
+        }
+    }
 }
